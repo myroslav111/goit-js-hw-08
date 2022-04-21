f0adb9957b9a4248907b0685804d6f555b872139
myroslav <myrkozar@gmail.com>
2022-04-21 16:40:40 +0300

    first task done

--- source/js/02-video.js
+++ source/js/02-video.js
@@ -1,1 +1,11 @@
+console.log(localStorage);
+
+localStorage.setItem('my-data', JSON.stringify({name: 'mango', age: 2}))
+
+const saveData = localStorage.getItem('my-data')
+console.log(saveData);
+
+const parseData = JSON.parse(saveData)
+console.log('parseData' , parseData);
+
 
