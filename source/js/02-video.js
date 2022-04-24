685dfc82c4b49f6e938ba943663c56d9ba99d886
myroslav <myrkozar@gmail.com>
2022-04-24 14:31:43 +0300

    hw is done

--- source/js/02-video.js
+++ source/js/02-video.js
@@ -1,11 +1,37 @@
-console.log(localStorage);
 
-localStorage.setItem('my-data', JSON.stringify({name: 'mango', age: 2}))
+// инициализировали vimeo
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
-const saveData = localStorage.getItem('my-data')
-console.log(saveData);
+// добавили библиотеку лодаш тротл
+const throttle = require('lodash.throttle');
 
-const parseData = JSON.parse(saveData)
-console.log('parseData' , parseData);
+// ключ локалсторидж
+const STORAGE_KEY_VIDEO = 'videoplayer-current-time';
 
+// запускаем отслеживание и сохранение времени плеера
+player.on('timeupdate', throttle(saveDataTime, 1000));
 
+// фун. сохранения currentTime в localStorage
+function saveDataTime(data) {
+    const saveTime = data.seconds;
+    localStorage.setItem(STORAGE_KEY_VIDEO, saveTime)
+    
+}
+
+// добавляем слушателя на плеер 
+player.on('play', triggeredVideoFromLastSaveTime)
+
+// фун. запуска видео с последнего сохраненного места
+function triggeredVideoFromLastSaveTime(data) {
+    const lastTime = localStorage.getItem(STORAGE_KEY_VIDEO)
+    if(lastTime){
+        player.setCurrentTime(Number(lastTime))
+    }
+    
+}
+
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
+console.log();
--- source/js/03-feedback.js
+++ source/js/03-feedback.js
@@ -1,25 +1,32 @@
+// добавили библиотеку лодаш тротл
 const throttle = require('lodash.throttle');
 
+// ключ локалсторидж
 const STORAGE_KEY = 'feedback-form-state'
 
+// линки на елементы форм
 const refs = {
     form: document.querySelector('.feedback-form'),
     input: document.querySelector('input'),
     textarea: document.querySelector('textarea'),
 };
 
+//добавили слушателя на форму
 refs.form.addEventListener('submit', onFormSabmit)
 refs.form.addEventListener('input', throttle(onInputareaInput, 500) )
 
 populateTextarea()
 
+// обьект хранилище для данных localStorage
 const formData = {}
 
+// фун. записи текущих данных в formData внесенных юзером
 function onInputareaInput(event) {
     formData[event.target.name] = event.target.value
     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData))
 }
 
+// фун. очистки данных с инпутов и localStorage
 function onFormSabmit(event) {
     event.preventDefault()
     console.log(formData);
@@ -28,6 +35,7 @@ function onFormSabmit(event) {
     
 }
 
+// фун. востановки данных в полях ввода
 function populateTextarea() {
     const saveMessege = JSON.parse(localStorage.getItem(STORAGE_KEY))
 
