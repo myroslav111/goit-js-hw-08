5b5e4b60626738057e2de64764bf8e838400436a
myroslav <myrkozar@gmail.com>
2022-04-26 20:27:31 +0300

    update

--- source/js/03-feedback.js
+++ source/js/03-feedback.js
@@ -15,20 +15,25 @@ const refs = {
 refs.form.addEventListener('submit', onFormSabmit)
 refs.form.addEventListener('input', throttle(onInputareaInput, 500) )
 
-populateTextarea()
-
 // обьект хранилище для данных localStorage
-const formData = {}
+const formData = {} 
 
 // фун. записи текущих данных в formData внесенных юзером
 function onInputareaInput(event) {
-    formData[event.target.name] = event.target.value
+    formData.email = event.currentTarget.email.value
+    formData.message = event.currentTarget.message.value
+    // formData[event.target.name] = event.target.value
     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData))
 }
 
 // фун. очистки данных с инпутов и localStorage
 function onFormSabmit(event) {
     event.preventDefault()
+
+    if(refs.input.value === '' || refs.textarea.value === ''){
+        return alert('all text fields must be filled')
+    }
+
     console.log(formData);
     event.currentTarget.reset()
     localStorage.removeItem(STORAGE_KEY)
@@ -36,11 +41,10 @@ function onFormSabmit(event) {
 }
 
 // фун. востановки данных в полях ввода
+populateTextarea()
 function populateTextarea() {
     const saveMessege = JSON.parse(localStorage.getItem(STORAGE_KEY))
 
-    if(saveMessege){
-        refs.textarea.value = saveMessege.message
-        refs.input.value = saveMessege.email
-    }
+    refs.textarea.value = saveMessege.message
+    refs.input.value = saveMessege.email
 }
